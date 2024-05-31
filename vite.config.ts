37da23107f32fa61bebd46cb29c04d53d1a1cc31
Adam Jurczyk <ajurczyk@evolution.com>
2024-06-01 01:11:22 +0200

    fix build configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from "path";
 
 export default defineConfig({
-    base: '/lospecjam2/',
+    base: '',
     resolve: {
         alias: {
             "~": resolve(__dirname, "src"),
